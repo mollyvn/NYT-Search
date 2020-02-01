ec6d0c8ca677bc2752bda3e88267beffa11443f1
Steffield <steffilebby@gmail.com>
2020-02-01 13:02:21 -0500

    no succesful changes

--- 16-NYTSearch/assets/js/javascript.js
+++ 16-NYTSearch/assets/js/javascript.js
@@ -8,16 +8,38 @@ var startYear =$("#start-year");
 var endYear =$("#end-year");
 var submitBtn =$("#submit");
 
+
+// $(submitBtn).on("click", function() {
+//     enteredSearchTerm = $(this).attr("data-search");
+//     var queryURL = "https://api.nytimes.com/svc/"
+//      + enteredSearchTerm + "/v2/articlesearch.json?q=election&api-key=" + APIkey;
+
+// //     $.ajax({
+// //       url: queryURL,
+// //       method: "GET"
+// //     })
+// //       .then(function(response) {
+// //         console.log(response);
+
+
+// //         for (var i = 0; i < response.docs[i].length; i++) {
+// //             var search = response.docs[i].keywords;
+// //             console.log(search);
+// //         };
+      
+//   });
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
 
+        
+
+        console.log(response);
         console.log(response.copyright);
-        for(var i=0; i<response.docs.length; i++){
-            console.log(response.docs[i].keywords);
-            console.log(response.docs[i].pub_date);
+        for(var i=0; i<response.docs[i].length; i++){
+            console.log(response.docs[1].keywords);
+            console.log(response.docs[1].pub_date);
         }
     });
 
@@ -25,4 +47,4 @@ var submitBtn =$("#submit");
 
     
 
-// )};
+// });
